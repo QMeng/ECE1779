f01eec4ba2aa52bab4c1a0eec794e132b58e1696
QMENG <damon.q.meng@gmail.com>
2018-09-25 20:17:14 -0400

    loginPage finished
    sign up page finished
    added User model
    Mysql db integreted
    database schema updated

--- app/config.py
+++ app/config.py
@@ -7,3 +7,5 @@ class Config(object):
     MYSQL_DATABASE_PASSWORD = "ece1779pass"
     MYSQL_DATABASE_DB = "ECE1779A1"
     MYSQL_CURSORCLASS = "DictCursor"
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:ece1779pass@localhost/ECE1779A1'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
---
+++ app/models.py
@@ -0,0 +1,28 @@
+from werkzeug.security import *
+from app import *
+from flask_login import UserMixin
+
+
+class User(UserMixin, db.Model):
+    __tablename__ = "UserInfo"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(128), unique=True)
+    email = db.Column(db.String(256), unique=True)
+    password = db.Column(db.String(256))
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
