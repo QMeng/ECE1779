6609a3772b00f10d88adba6e1d3866e410770ae3
QMENG <damon.q.meng@gmail.com>
2018-11-09 11:12:43 -0500

    manual scaling up and down finished, create instance finsihed, destroy instance finished, remote db connected, cpu table finished

--- imageUploader.py
+++ imageUploader.py
@@ -1,4 +1,4 @@
 from app_worker import app_worker
 
 if __name__ == "__main__":
-    app_worker.run(host='0.0.0.0', port=80)
+    app_worker.run(host='0.0.0.0', port=8080)
--- run_worker.sh
+++ run_worker.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 source venv/bin/activate
-gunicorn -w 4 --worker-class gevent --access-logfile access.log --error-logfile error.log -b 0.0.0.0:80 app:app_worker
+gunicorn -w 4 --worker-class gevent --access-logfile access.log --error-logfile error.log -b 0.0.0.0:8080 app:app_worker
