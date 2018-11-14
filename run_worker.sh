68e2a8637ee7c3e17c72fc355193f1c4f5ead8de
QMENG <damon.q.meng@gmail.com>
2018-11-14 11:03:39 -0500

    minor change in run worker sh

--- run_worker.sh
+++ run_worker.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-gunicorn -w 4 --worker-class gevent --access-logfile access.log --error-logfile error.log -b 0.0.0.0:8080 app_worker:app_worker
+gunicorn -w 4 --worker-class gevent --access-logfile access.log --error-logfile error.log -b 0.0.0.0:8080 imageUploader:app_worker
