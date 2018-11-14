4ba69cd987b56fc158f36c16b35ae09ac3ecadce
QMENG <damon.q.meng@gmail.com>
2018-11-13 23:36:31 -0500

    minor change in run worker

--- run_worker.sh
+++ run_worker.sh
@@ -1,4 +1,2 @@
-#cloud-boothook
-#!/bin/bash
-source venv/bin/activate
+#!/usr/bin/env bash
 gunicorn -w 4 --worker-class gevent --access-logfile access.log --error-logfile error.log -b 0.0.0.0:8080 app_worker:app_worker
