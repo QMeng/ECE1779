58416518b4322937cc185a2a4386272406962ac0
QMENG <damon.q.meng@gmail.com>
2018-11-14 20:40:35 -0500

    minor fix in run worker

--- run_worker.sh
+++ run_worker.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-source venv/bin/activate
-gunicorn -w 4 --worker-class gevent --access-logfile access.log --error-logfile error.log -b 0.0.0.0:8080 imageUploader:app_worker
+. venv/bin/activate
+gunicorn -w 4 --worker-class gevent --access-logfile access.log --error-logfile error.log -b 0.0.0.0:8080 app_worker:app_worker
