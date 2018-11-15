9ce951fa84033a5442757aed6f22e1a449ba9bf1
QMENG <damon.q.meng@gmail.com>
2018-11-14 21:08:48 -0500

    more bash scripts

--- run_worker_dot.sh
+++ run_worker_dot.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-source venv/bin/activate
+. venv/bin/activate
 gunicorn -w 4 --worker-class gevent --access-logfile access.log --error-logfile error.log -b 0.0.0.0:8080 app_worker:app_worker
